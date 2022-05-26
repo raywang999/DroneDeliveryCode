570a8002a04325955cdc6c9b1240d24d3746263c
K12573 <75595156+K12573@users.noreply.github.com>
2022-05-25 22:43:48 -0400

    Rough draft of how to control the drone autonomously

--- drone_controller.py
+++ drone_controller.py
@@ -7,34 +7,46 @@ from djitellopy import tello
 
 drone = tello.Tello()
 
+path_coords = []
+path_dist = []
+turning_angle = []
+
 class drone_controller:
-    path_coords = []
-    path_dist = []
 
     def __init__(self):
         f = open('waypoints.json', "r")
         data = json.loads(f.read())    
         for i in data["pos"]:
-            self.path_coords.append(i)
+            path_coords.append(i)
 
         for i in data["wp"]:
-            self.path_dist.append(i)
+            path_dist.append(i["dist_cm"])
     
+        for i in data["wp"]:
+            turning_angle.append(i["angle_deg"])
+        
     def turn_left(deg):
         drone.rotate_counter_clockwise(180-deg)
 
     def turn_right(deg):
         drone.rotate_clockwise(180-deg)
-
-    def get_path_coords():
-        return path_coords
-
-    def get_path_dist():
-        return path_dist    
     
-
-
 d = drone_controller()
+
+print("length path_coords: ", len(path_coords))
+print("length path_dist: ", len(path_dist))
+print(turning_angle)
+
+# loop through the coordinate points and adjust the drone's direction accordingly
+for i in range (1,len(path_coords)+1):
+    if path_coords[i][0] > path_coords[i-1][0]:
+        d.turn_right(turning_angle[i-1])
+    else:
+        d.turn_left(turning_angle[i-1])
+    
+    drone.move_forward(path_dist[i-1])
+
+drone.land()
 
 
 
