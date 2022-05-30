593023f21db684f2bd0d64a37e0521595df8190b
K12573 <75595156+K12573@users.noreply.github.com>
2022-05-29 21:17:45 -0400

    Final Code - Done!!!

--- basicMovements.py
+++ basicMovements.py
@@ -3,7 +3,7 @@ from djitellopy import tello
 drone = tello.Tello()
 
 # connect to drone
-drone.connect()
+drone.connect() 
 print("Battery Level: ", drone.get_battery())
 # drone.takeoff()
 # drone.rotate_counter_clockwise(90)
